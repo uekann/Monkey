b803e5bdfa2139008adace4d2f31f9c16f23fb30
uekann <kantaueno0808@yahoo.co.jp>
2024-02-17 04:40:48 +0900

    構文解析のreplの実装

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
 use Monkey::repl;
 
+const MONKEY_FACE: &str = r#"
+            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"`` ``"-./, -' /
+   `'-' /_   ^ ^   _\ '-'`
+       |  \._   _./  |
+       \   \ `~` /   /
+        '._ '---' _.'
+           '~---~'
+
+"#;
+
 fn main() {
+    println!("{}", MONKEY_FACE);
     println!("Hello! This is the Monkey programming language!");
     println!("Feel free to type in commands");
+
     repl::start();
+    println!("Goodbye!")
 }
--- src/repl.rs
+++ src/repl.rs
@@ -1,29 +1,54 @@
+use crate::ast::Program;
 use crate::lexer::Lexer;
+use crate::parser::Parser;
 use crate::token::TokenType;
 use std::io::{self, Write};
 
 const PROMPT: &str = ">> ";
+// pub fn start() {
+//     loop {
+//         let mut input = String::new();
+//         print!("{}", PROMPT);
+//         io::stdout().flush().unwrap();
+//         io::stdin().read_line(&mut input).unwrap();
+//         let mut l = Lexer::new(&input);
+
+//         // 最初のトークンがEOFなら終了
+//         let tok = l.next_token();
+//         if tok.token_type == TokenType::EOF {
+//             break;
+//         }
+//         println!("{:?}", tok);
+
+//         loop {
+//             let tok = l.next_token();
+//             if tok.token_type == TokenType::EOF {
+//                 break;
+//             }
+//             println!("{:?}", tok);
+//         }
+//     }
+// }
+
 pub fn start() {
     loop {
         let mut input = String::new();
         print!("{}", PROMPT);
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut input).unwrap();
-        let mut l = Lexer::new(&input);
-
-        // 最初のトークンがEOFなら終了
-        let tok = l.next_token();
-        if tok.token_type == TokenType::EOF {
+        if input.trim() == "exit" {
             break;
         }
-        println!("{:?}", tok);
-
-        loop {
-            let tok = l.next_token();
-            if tok.token_type == TokenType::EOF {
-                break;
+        let l = Lexer::new(&input);
+        let mut p = Parser::new(l);
+        let program = p.parse_program();
+        match program {
+            Ok(p) => {
+                println!("{}", p);
             }
-            println!("{:?}", tok);
+            Err(e) => {
+                println!("Error: {}", e);
+            }
         }
     }
 }
